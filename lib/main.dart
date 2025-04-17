d909fef13c699b0b6c354ae157a6fbb60d5d8729
manta33 <sksmstmdals3@gmail.com>
2025-04-17 16:51:05 +0900

    chore(firebase): set up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:daily_planner/features/home/home_screen.dart';
+import 'package:daily_planner/firebase_options.dart';
 import 'package:daily_planner/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const DailyPlanner());
 }
 
