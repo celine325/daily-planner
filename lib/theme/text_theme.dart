4211d51c13c52504b388cad7f8d99ab51db19a36
manta33 <sksmstmdals3@gmail.com>
2025-04-17 17:53:37 +0900

    feat(theme): apply TextTheme

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme {
+  static const TextTheme base = TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 48,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+}
